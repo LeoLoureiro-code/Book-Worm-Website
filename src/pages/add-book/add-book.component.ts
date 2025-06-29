4afa52038b704e90f4f0b255b894c2bf00b86b7f
Leo Loureiro <leolou.90@gmail.com>
2025-06-28 23:26:36 -0700

    add book form and book state to add book component

--- src/pages/add-book/add-book.component.ts
+++ src/pages/add-book/add-book.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { BookFormComponent } from "../../components/book-form/book-form.component";
+import { BookStateComponent } from "../../components/book-state/book-state.component";
 
 @Component({
   selector: 'app-add-book',
   standalone: true,
-  imports: [],
+  imports: [BookFormComponent, BookStateComponent],
   templateUrl: './add-book.component.html',
   styleUrl: './add-book.component.scss'
 })

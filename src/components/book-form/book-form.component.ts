db1727cdc046f39df2162a948b385c6226c64de1
Leo Loureiro <leolou.90@gmail.com>
2025-06-29 19:06:48 -0700

    add tags and form styles

--- src/components/book-form/book-form.component.ts
+++ src/components/book-form/book-form.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-book-form',
+  selector: 'book-form',
   standalone: true,
   imports: [],
   templateUrl: './book-form.component.html',

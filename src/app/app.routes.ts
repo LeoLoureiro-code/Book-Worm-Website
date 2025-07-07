5304b0bb9689db4e2729f47a9af1678bf967973b
Leo <leolou.90@gmail.com>
2025-07-06 19:37:11 -0700

    add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { AddBookComponent } from '../pages/add-book/add-book.component';
+import { LibraryComponent } from '../pages/library/library.component';
+import { LoginComponent } from '../pages/login/login.component';
+import path from 'path';
+import { SignUpComponent } from '../pages/sign-up/sign-up.component';
+import { EditBookComponent } from '../pages/edit-book/edit-book.component';
+
+
+export const routes: Routes = [
+
+    { path: '', component: LoginComponent },
+    {path: 'login', component: LoginComponent},
+    { path: 'signup', component: SignUpComponent},
+    { path: 'add', component: AddBookComponent },
+    {path: 'edit', component: EditBookComponent},
+    { path: 'library', component: LibraryComponent },
+    { path: '**', redirectTo: '', pathMatch: 'full' }
+];

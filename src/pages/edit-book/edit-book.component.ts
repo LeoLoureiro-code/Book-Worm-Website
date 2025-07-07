3ea73dd872058bb5bc798c7bd045fdd1b5769dda
Leo <leolou.90@gmail.com>
2025-07-06 19:46:59 -0700

    add book emotion rate component

--- src/pages/edit-book/edit-book.component.ts
+++ src/pages/edit-book/edit-book.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { BookFormComponent } from '../../components/book-form/book-form.component';
 import { BookStateComponent } from '../../components/book-state/book-state.component';
+import { BookEmotionRateComponent } from '../../components/book-emotion-rate/book-emotion-rate.component';
 
 @Component({
   selector: 'app-edit-book',
   standalone: true,
-  imports: [BookFormComponent, BookStateComponent],
+  imports: [BookFormComponent, BookStateComponent, BookEmotionRateComponent],
   templateUrl: './edit-book.component.html',
   styleUrl: './edit-book.component.scss'
 })

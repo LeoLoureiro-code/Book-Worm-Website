22d6aad3ce51b2a2018cbfef853af5dc691a826f
Leo <leolou.90@gmail.com>
2025-10-05 18:15:02 -0700

    add page to edit books in the library

---
+++ src/pages/edit-book-page/edit-book-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-book-page',
+  imports: [],
+  templateUrl: './edit-book-page.html',
+  styleUrl: './edit-book-page.scss'
+})
+export class EditBookPage {
+
+}
